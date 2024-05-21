c7dac441a8ac8a497a0c11f1e1158a0b75000b0b
Jonas <jonas.wittler@gmail.com>
2024-05-21 13:38:33 +0200

    added homemanager

--- flake.nix
+++ flake.nix
@@ -4,30 +4,35 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-     #home-manager = {
-     #  url = "github:nix-community/home-manager";
-     #  inputs.nixpkgs.follows = "nixpkgs";
-     #};
+     home-manager = {
+       url = "github:nix-community/home-manager";
+       inputs.nixpkgs.follows = "nixpkgs";
+     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: 
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: 
    let
     system = "x86_64-linux";
     hostname = "nixos-vm";
     username = "jonas";
-	pkgs = nixpkgs.legacyPackages.${system};
+	  pkgs = nixpkgs.legacyPackages.${system};
    in
    {
     imports = [
         modules/nixos/general.nix
     ];
-     nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
-       specialArgs = {inherit inputs;};
-       modules = [
-          hosts/${hostname}/configuration.nix
-          
-          #inputs.home-manager.nixosModules.${hostname}
-       ];
-      };
+    nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        hosts/${hostname}/configuration.nix
+
+        home-manager.nixosModules.home-manager 
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.${username} = import modules/home-manager/home.nix;
+        }
+      ];
+    };
   };
 }
